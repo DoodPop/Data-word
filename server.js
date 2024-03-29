5b70c91bdbcaff861ddcd27400033cf711666013
DoodPop <117036115+DoodPop@users.noreply.github.com>
2024-03-29 16:01:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ function getRandomDistractors(words, count) {
   return selectedWords;
 }
 
-app.listen(10000, () => {
-    console.log("running on port 10000");
+app.listen(3001, () => {
+    console.log("running on port 3001");
 });
